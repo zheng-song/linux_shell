b79709ad840e4dd4926ca53d367e6affb89e563a
zhengsong <741633710@qq.com>
2017-04-25 18:56:43 +0800

    modify

---
+++ linux_shell/linux_start.sh
@@ -0,0 +1,3 @@
+#linux start with init process,and the create other process,init used to initialization linux system,and set stdin、stdout、stderr
+#linux init process responsible for execute the shell script under /etc/rc.d/init.d,those Script are set according run level
+#we can use "chkconfig" to set whether auto execute Script above.
