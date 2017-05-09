c9975d50a141db854f470fc25af5f2cf989c7b4e
zs <741633710@qq.com>
2017-05-09 08:20:29 +0800

    add hello_world.c.bak1

--- vxworks/hello_world.c
+++ vxworks/hello_world.c
@@ -41,36 +41,118 @@ LOCAL　STATUS usbTestPhysDevCreate(
 
 void testUSBInit (void)
 {
-    printf("hello USB test");
-    if(usbdInit() != USBHST_SUCCESS){
-    	exit(ERROR);
-    }
-    printf("usbdInit successful\n");
+   printf("hello USB test\n");
+//==========================================================
 
-    if (usbHubInit() != 0){
-    	exit(ERROR);
-    }
-    printf("usbdHubInit successful\n");
+   // if (usbInit() != OK){
+   // 	printf("usbInit failed!\n" );
+   // 	return ERROR;
+   // }else{
+   // 	printf("usb init OK\n");
+   // }
+//不需要这一步，usbdInitialize()中已经包含了usbInit()这一步。
+//==========================================================
 
-    
+   if (usbdInitialize() != OK){
+   	printf("Failed to initialize USBD\n");
+   	return ERROR;
+   }else{
+   	printf("USBD initialize success\n");
+   	}
+
+//挂接过程
+   	UINT8 busNO;
+   	UINT8 deviceNo;
+   	UINT8 funcNo;
+   	PCI_CFG_HEADER pciCfgHdr;
+   	GENERIC_HANDLE uhciAttachToken;
+   	HCD_EXEC_FUNC hcdExecFunc;
+
+   	if (!usbPciClassFind(UHCI_CLASS,UHCI_SUBCLASS,UHCI_PGMIF,0,&busNo,&deviceNo,&funcNo)){
+   		return ERROR;
+   	}
+   	printf("usbPciClassFind success!\n");
+
+   	usbPciConfigHeaderGet(busNo,deviceNo,funcNo,&pciCfgHdr);
+   	/* Attach the UHCI HCD to the USBD.The function usbHcdUhci() is exported by usbHcdUhciLib.*/
+   	if (usbHcdAttach(usbHcdUhciExec,&pciCfgHdr,&uhciAttachToken) != OK){
+   		return ERROR;
+   	}
+   	printf("usbHcdAttach OK!\n");
+
+
+// STATUS usbdHcdAttach(HCD_EXEC_FUNC hcdExecFunc,pVOID param, pGENERIC_HANDLE pattachToken);
+   // usbHcdAttach()
+
+// STATUS usbdclientregister(pCHAR pClientName, 
+//		pUSBD_CLIENT_HANDLE pClientHandle /*Client hdl returned by USBD*/ );
+   pUSBD_CLIENT_HANDLE pUSB_TTYHandle;
+  if (usbdClientRegister("USB/TTY",&pUSB_TTYHandle) != OK){
+  	printf("usbdclientregister failed\n");
+  	return ERROR;
+  }
+  printf("usbdclientregister OK!\n");
+
+   if (usbdDynamicAttachRegister(pUSB_TTYHandle,USB_TEST_CLASS,USB_TEST_SUB_CLASS,
+   	USB_TEST_DRIVE_PROTOCOL,TRUE,usbTestDevAttachCallback ) != OK){
+   printf("usbdDynamicAttachRegister failed !\n");   	
+   return ERROR;
+   }
+   printf("usbdDynamicAttachRegister success!\n");
 
 
 
+    // if(usbdInitialize() != OK)
+    // {
+    // 	printf("can not init usbd\n");
+    // 	exit(0);
+    // }
+
+    // if(usbdClientRegister("USB_TEST"，&usbdClientHandle) != OK || 
+    // 	usbdDynamicAttachRegister(usbdClientHandle,USB_TEST_CLASS,USB_TEST_SUB_CLASS,
+    // 		USB_TEST_DRIVE_PROTOCOL,usbTestDevAttachCallback) != OK){
+
+    // }
+
+}
 
 
 
-    if(usbdInitialize() != OK)
-    {
-    	printf("can not init usbd\n");
-    	exit(0);
-    }
+//==========================USB INIT
 
-    if(usbdClientRegister("USB_TEST"，&usbdClientHandle) != OK || 
-    	usbdDynamicAttachRegister(usbdClientHandle,USB_TEST_CLASS,USB_TEST_SUB_CLASS,
-    		USB_TEST_DRIVE_PROTOCOL,usbTestDevAttachCallback) != OK){
-
-    }
-
+UINT16 cmdUsbInit()/*Initialize USBD*/
+{
+	UINT16 usbdVersion;
+	char usbdMfg[USBD_NAME_LEN+1];
+	UINT16 s;
+	/*if alreadly initialized,just show a warning*/
+	if (initialized){
+		printf("Already initialized.\n");
+		return RET_CONTINUE;
+	}
+	/*Initialize the USBD*/
+	s=usbdInitialize();
+	printf("usbdInitialize returned %d\n",s);
+	if (s == OK){
+		s=usbdClientRegister(PGM_NAME,&usbdClientHandle);
+		printf("usbdClientRegister returned %d\n",s);
+		if (s == OK){
+			printf("usbdClientHandle = 0x%x\n",(UINT32)usbdClientHandle);
+			/*Display the USBD version*/
+			if ((s=usbdVersionGet(&usbdVersion,usbdMfg)) != OK){
+				printf("usbdVersionGet() returned %d\n",s);
+			}else{
+				printf("USBD version=0x%5.4x\n",usbdVersion);
+				printf("USBD mfg='%s'\n",usbdMfg);
+			}
+			if (s == OK)
+				initialized = TRUE;
+		}
+	}
+	if (s!=OK){
+		printf("Initialized failed!\n");
+	}
+	return RET_CONTINUE;
 }
 
 
